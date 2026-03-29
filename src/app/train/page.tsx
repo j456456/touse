5f50302e6f0ec28ae6cc61b5a53bf199f04c9f56
Haotian <panhaotian@gmail.com>
2026-03-29 08:21:53 -0400

    put things

--- src/app/train/page.tsx
+++ src/app/train/page.tsx
@@ -3,11 +3,9 @@ import TrainDashboard from "@/components/train/TrainDashboard";
 
 export default function TrainPage() {
   return (
-    <div className="h-screen bg-parchment p-4 md:p-6">
-      <div className="h-full border-[3px] border-celadon rounded-2xl overflow-hidden bg-parchment flex flex-col">
-        <Navbar />
-        <TrainDashboard />
-      </div>
+    <div className="h-screen bg-parchment flex flex-col">
+      <Navbar />
+      <TrainDashboard />
     </div>
   );
 }
--- src/app/translate/page.tsx
+++ src/app/translate/page.tsx
@@ -3,11 +3,9 @@ import ChatInterface from "@/components/translate/ChatInterface";
 
 export default function TranslatePage() {
   return (
-    <div className="h-screen bg-parchment p-4 md:p-6">
-      <div className="h-full border-[3px] border-celadon rounded-2xl overflow-hidden bg-parchment flex flex-col">
-        <Navbar />
-        <ChatInterface />
-      </div>
+    <div className="h-screen bg-parchment flex flex-col">
+      <Navbar />
+      <ChatInterface />
     </div>
   );
 }
