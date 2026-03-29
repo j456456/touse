1e167db1f01a3b92f39bfca54d1af3793da15771
Haotian <panhaotian@gmail.com>
2026-03-29 07:24:05 -0400

    change things

---
+++ src/lib/fonts.ts
@@ -0,0 +1,12 @@
+import { Lora, Rock_Salt } from "next/font/google";
+
+export const lora = Lora({
+  variable: "--font-lora",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const rockSalt = Rock_Salt({
+  subsets: ["latin"],
+  weight: ["400"],
+});
